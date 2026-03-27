59f7a9320457c47f81b547d2dec5dca7074cda7e
Saad-Ismailwala <saadismailwala01@gmail.com>
2026-03-27 17:10:36 +0530

    2751026

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
   app.use(express.static(frontendPath));
 
